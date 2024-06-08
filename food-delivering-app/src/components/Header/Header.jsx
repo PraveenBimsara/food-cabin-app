6a73db16cd5995724909a1d37c0bcdf56bfc6091
PraveenBimsara <praveenbimsara99@gmail.com>
2024-06-08 11:21:50 +0530

    Initial Commit(Navbar,Header,Food list)

---
+++ food-delivering-app/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Discover a wide variety of cuisines and dishes, meticulously prepared to satisfy your cravings and delivered hot and fresh right to your door.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
