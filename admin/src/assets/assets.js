4638794085d2a2d3c014fbcba2f8ae534b630dc7
PraveenBimsara <praveenbimsara99@gmail.com>
2024-07-14 21:52:04 +0530

    Creating admin panel

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,13 @@
+import add_icon from './add_icon.jpg'
+import order_icon from './order_icon.jpg'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+}
